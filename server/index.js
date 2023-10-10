cb63c12db7983106e5ea13b03f3093ab54db45d7
Thallis Abreu da Silva <177450@upf.br>
2023-10-09 22:11:33 -0300

    estudando back node js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,12 @@
-console.log("hello, world");
-// print('hello, world');
-
 const express = require('express');
-//import 'package:express/express.dart';
-const PORT = 3000;
 
+const authRouter = require('./routes/auth');
+
+const PORT = 3000;
 const app = express();
 
-//creating an api
-app.get('/hello-world', (req, res)=>{
-    res.send("aaaaaaaaaaa hw")
-})
-// GET, PUT, POST, DELETE, UPDATE -> CRUD
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`connected at port' ${PORT} hello`);
+app.use(authRouter);
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);
 });
