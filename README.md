ac831f3e777d50e6b29134718bd9b4afdacaf7c8
Thallis Abreu da Silva <177450@upf.br>
2023-11-27 19:57:17 -0300

    README

--- README.md
+++ README.md
@@ -15,3 +15,39 @@ O **ECOM** é um aplicativo de compras e-commerce desenvolvido com as tecnologia
 - [Mongoose](https://mongoosejs.com/)
 - [Cloudinary](https://cloudinary.com/)
 - [Provedor (Flutter State Management)](https://pub.dev/packages/provider)
+
+## Features
+- Autenticação de e-mail e senha
+- Estado de autenticação persistente
+- Pesquisando produtos
+- Filtragem de produtos (com base na categoria)
+- Detalhes do produto
+- Avaliação
+- Obtendo a oferta do dia
+- Carrinho
+- Sair
+- Painel de administração
+- Visualizando todos os produtos
+- Adicionando Produtos
+- Excluindo Produtos
+
+### Lado do servidor
+```bash
+  cd server
+  npm install
+  npm run dev (for continuous development)
+  OR
+  npm start (to run script 1 time)
+```
+
+### Lado do cliente
+```bash
+  flutter pub get
+  open -a simulator (to get iOS Simulator)
+  flutter run
+```
+
+## Tecnologia usada
+**Servidor**: Node.js, Express, Mongoose, MongoDB, Cloudinary
+
+**Cliente**: Flutter, Provider
--- lib/home/widgets/top_categories.dart
+++ lib/home/widgets/top_categories.dart
@@ -14,41 +14,43 @@ class TopCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 60,
-      child: ListView.builder(
-        itemCount: GlobalVariables.categoryImages.length,
-        scrollDirection: Axis.horizontal,
-        itemExtent: 75,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () => navigateToCategoryPage(
-              context,
-              GlobalVariables.categoryImages[index]['title']!,
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Image.asset(
-                      GlobalVariables.categoryImages[index]['image']!,
-                      fit: BoxFit.cover,
-                      height: 40,
-                      width: 40,
+      child: Center(
+        child: ListView.builder(
+          itemCount: GlobalVariables.categoryImages.length,
+          scrollDirection: Axis.horizontal,
+          itemExtent: 75,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () => navigateToCategoryPage(
+                context,
+                GlobalVariables.categoryImages[index]['title']!,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        GlobalVariables.categoryImages[index]['image']!,
+                        fit: BoxFit.cover,
+                        height: 40,
+                        width: 40,
+                      ),
                     ),
                   ),
-                ),
-                Text(
-                  GlobalVariables.categoryImages[index]['title']!,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
+                  Text(
+                    GlobalVariables.categoryImages[index]['title']!,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          );
-        },
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
